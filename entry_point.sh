4bd02960af60277aed0947644d3b00f49063be0f
Barys <widapro@users.noreply.github.com>
2019-02-15 20:13:47 +0300

    initial commit

---
+++ entry_point.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+_cleanup() {
+        echo "Stopping services..."
+
+        service avreg stop
+        service apache2 stop
+        service mysql stop
+        service memcached stop
+
+        kill -s SIGTERM $!
+
+        echo "...container stopped."
+
+        exit 0
+}
+
+echo "Starting services..."
+
+# remove any service pids in case if container was incorrectly killed
+service avreg stop
+service apache2 stop
+service mysql stop
+service memcached stop
+
+service mysql start
+service apache2 start
+service avreg start
+service memcached start
+
+echo "...services started."
+
+trap _cleanup SIGTERM
+trap _cleanup SIGINT
+
+while [ 1 ]
+do
+  sleep 60 &
+  wait $!
+done
